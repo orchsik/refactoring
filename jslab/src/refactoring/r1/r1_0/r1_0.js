15f9f49c777f2a67b2aadf93a16444b6a7dee267
orchsik <orchsik@naver.com>
2021-04-08 22:34:00 +0900

    Add: htmlstatement

--- jslab/src/refactoring/r1/r1_0/r1_0.js
+++ jslab/src/refactoring/r1/r1_0/r1_0.js
@@ -2,6 +2,24 @@ const invoices = require('../invoices.json');
 const plays = require('../plays.json');
 const { createStatementData } = require('./createStatementData');
 
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += '<table>\n';
+  result += '<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>';
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td> `;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += '</table>\n';
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em></p>\n`;
+  return result;
+}
+
 function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
@@ -33,4 +51,5 @@ function usd(aNumber) {
 
 module.exports = {
   statement,
+  htmlStatement,
 };
