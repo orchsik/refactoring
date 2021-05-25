25b73c3001d476cb75b224aecafa40c560de9dc0
orchsik <orchsik@naver.com>
2021-05-26 00:11:37 +0900

    FIx: Node Buffer

--- jslab/src/node-fundmental/Buffer/04-write.js
+++ jslab/src/node-fundmental/Buffer/04-write.js
@@ -4,23 +4,18 @@ var result;
 buffer = Buffer.alloc(16);
 
 result = buffer.write('Hello', 'utf8');
-console.log(1, result);
-// 5
-// write에 사용한 byte 개수
+console.log(result); // 5 (write에 사용한 byte 개수)
 
-result = buffer.write(' world!', 5, 'utf8');
 // 두번째 인자를 주면 해당 인덱스부터 write.
+buffer.write(' world!', 5, 'utf8');
+console.log(buffer.toString('utf8')); // Hello world!
 
-console.log(2, buffer.toString('utf8'));
-// Hello world!
-console.log(3, buffer.toString('utf8', 0, 5));
-// Hello
 // write범위: 시작인덱스, 끝인덱스
+console.log(buffer.toString('utf8', 0, 5)); // Hello
 
-// 배열처럼 줄 수 있음.
+// 배열처럼 요로코롬 줄 수도 있음.
 buffer[12] = buffer[11];
 buffer[13] = '!'.charCodeAt();
 buffer[14] = buffer[13];
 buffer[15] = 33;
-console.log(buffer.toString('utf8'));
-// Hello world!!!!!
+console.log(buffer.toString('utf8')); // Hello world!!!!!
--- jslab/src/node-fundmental/Buffer/05-more.js
+++ jslab/src/node-fundmental/Buffer/05-more.js
@@ -1,44 +1,39 @@
-var _log = (a) => console.log(a);
 var result;
 var buffer = Buffer.alloc(16);
 var santa = '🎅';
 
-// 너 Buffer냐?
-_log(Buffer.isBuffer(santa)); // false
+// 너 Buffer니?
+console.log(Buffer.isBuffer(santa)); // false
 
-// 필요한 Buffer 크기
-_log(santa.length); // 2
-_log(Buffer.byteLength(santa)); // 4
+// write할 때, 필요한 Buffer 크기
+console.log(Buffer.byteLength(santa)); // 4
 
 // 버퍼 전체 크기
-_log(buffer.write(santa)); // 4
-_log(buffer.length); // 16
+console.log(buffer.length); // 16
 
 /**
  * buffer.copy(target, targetStart=0, sourceStart=0, sourceEnd=buffer.length)
  */
 var myHome = Buffer.alloc(24);
 result = myHome.write('Merry Christmas!', 'utf8');
-_log(result); // 16
-// merry christmas!
-// 1234567890123456
+console.log(result); // 16
 
 var santa_buffer = Buffer.from(santa, 'utf-8');
 result = santa_buffer.copy(myHome, 16);
-_log(result); // 4
+console.log(result); // 4
 
-_log(myHome.toString('utf8', 0, 16 + 4));
+console.log(myHome.toString('utf8', 0, 16 + 4));
 // Merry Christmas!🎅
 
 /**
- * buffer.slice(start, end=buffer.length)
- */
-// Array.prototype.slice와 비슷.
-// 단! slice의 return값이 새로운 객체가 아니라 동일 객체임.
-// 수정하면 원본 Buffer도 수정된다.
+buffer.slice(start, end=buffer.length)
+Array.prototype.slice와 비슷하다.
+단! slice의 return값이 새로운 객체가 아니라 동일 객체다.
+수정하면 원본 Buffer도 수정된다.
+*/
 var father = myHome.slice(16, 20);
 console.log(father.toString()); // 🎅
 
 father.write('👨');
-console.log(myHome.toString()); // Merry Christmas!👨
 console.log(father.toString()); // 👨
+console.log(myHome.toString()); // Merry Christmas!👨
