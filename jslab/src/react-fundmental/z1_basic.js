3adca85f1a106437778ce380cb99ce41e8bade6c
orchsik <orchsik@naver.com>
2021-05-29 09:00:13 +0900

    Add: concurrentMode init

--- jslab/src/react-fundmental/z1_basic.js
+++ jslab/src/react-fundmental/z1_basic.js
@@ -36,11 +36,6 @@ function render(element, container) {
       dom[name] = element.props[name];
     });
 
-  /**
-   * 재귀호출이 문제임.
-   * 렌더링 시작하면 모든 element tree를 렌더링 할 때까지 멈추지 않음.
-   * element tree가 꽤 크면 메인 스레드의 동작이 오랫동안 멈춰있을꺼야.
-   */
   element.props.children.forEach((child) => render(child, dom));
 
   container.appendChild(dom);
@@ -62,8 +57,3 @@ const element = (
 const container = document.getElementById('root');
 
 MyReact.render(element, container);
-
-/**
- * 고고 render 리팩토링 가자!
- * 작업을 더 작은 단위로 나누고, 브라우저가 작업이 필요한 경우, 렌더링 도중 끼어들수 있게 하자.
- */
